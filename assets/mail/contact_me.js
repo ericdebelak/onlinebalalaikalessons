e49082c6365377fce9a6d9e54fdf7deced3383bc
Eric Debelak <eric.debelak@call2test.com>
2020-10-23 18:00:20 -0600

    Initial commit

---
+++ assets/mail/contact_me.js
@@ -0,0 +1,20 @@
+$(function () {
+    $(
+        "#contactForm input,#contactForm textarea,#contactForm button"
+    ).jqBootstrapValidation({
+        preventSubmit: true,
+        filter: function () {
+            return $(this).is(":visible");
+        },
+    });
+
+    $('a[data-toggle="tab"]').click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$("#name").focus(function () {
+    $("#success").html("");
+});
